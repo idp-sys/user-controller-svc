8c1400cfe846218a7ed7be7a3de62d46c9263d6d
Stenio Dinart <din-art@outlook.com>
2020-01-11 19:22:11 -0300

    Adição – Adicionado Entidade User. Adição – Adicionado diretórios Interfaces e Services Adição – Adicionado connection   string  para banco sql local

---
+++ UserManager.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserManager.Domain.Entities
+{
+   public class User
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
